747978b44b1f4e682660de38e1b8ba9d3abc713a
Travis Devitt <travis.devitt@gmail.com>
2021-10-15 15:47:05 -0500

    Update montecarlo.py
    finished building out GBM function and basic plotting

--- montecarlo.py
+++ montecarlo.py
@@ -17,12 +17,28 @@ def GeometricBrownianMotion(initial_price, drift, volatility, timestep, total_ti
 
     return prices
 
-price = 10.0
-annual_vol = 0.35
-timestep_in_years = 1 / 365
-timeframe_in_years = 1
-annual_drift = 0.10
+price = 10.0 #current price of underlying
+annual_vol = 0.40 #estimate of forward looking annualized volatility
+timestep_in_years = 1 / 252 #estimated trading days in calendar year is 252 
+timeframe_in_years = 5
+annual_drift = 0.10 #expected annual return (or alternatively the asset specific discount rate)
 
-price_path = GeometricBrownianMotion(price, annual_drift, annual_vol, timestep_in_years, timeframe_in_years)
+simulations = 100
+simulated_paths = []
+final_prices = []
 
-print(price_path)
+for i in range(0, simulations):
+    price_path = GeometricBrownianMotion(price, annual_drift, annual_vol, timestep_in_years, timeframe_in_years)
+    simulated_paths.append(price_path)
+    final_prices.append(price_path[-1])
+
+avg_final_price = np.average(final_prices)
+discounted_avg_final_price = avg_final_price / pow(1+annual_drift, timeframe_in_years)
+
+print(avg_final_price)
+print(np.median(final_prices))
+
+for sim in simulated_paths:
+    plt.plot(np.arange(0, len(sim)), sim)
+
+plt.show()
